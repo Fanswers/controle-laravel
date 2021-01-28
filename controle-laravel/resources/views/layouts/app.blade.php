ce18249e945747dd2a9df52a2ffe96858af3057b
Larrode Alexis <alexis.larrode@gmail.com>
2021-01-28 09:50:43 +0100

    :package: finish project setup

--- controle-laravel/resources/views/layouts/app.blade.php
+++ controle-laravel/resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
--- controle-laravel/webpack.mix.js
+++ controle-laravel/webpack.mix.js
@@ -14,6 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
-    .postCss("ressources/css/app.css", "public/css", [
-        require("tailindcss"),
-    ])
+    .postCss("resources/css/app.css", "public/css", [
+        require("tailwindcss"),
+    ]);
